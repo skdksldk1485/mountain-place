5a6d0b0deb7b8eb2de34d653f04365bdc9096bd1
skdksldk1485 <skdksldk1485@naver.com>
2022-02-27 17:11:23 +0900

    Setup DB, add JPA Data

--- src/main/java/com/mountain/place/SantanaApplication.java
+++ src/main/java/com/mountain/place/SantanaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlaceApplication {
+public class SantanaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlaceApplication.class, args);
+		SpringApplication.run(SantanaApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/mountain/place/domain/user/model/User.java
@@ -0,0 +1,27 @@
+package com.mountain.place.domain.user.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name ="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "user_img")
+    private String userImg;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
