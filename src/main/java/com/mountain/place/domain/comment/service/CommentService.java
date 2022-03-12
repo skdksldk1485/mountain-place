7866afb4144060e53cb03124883c687cc43d2beb
skdksldk1485 <skdksldk1485@naver.com>
2022-03-12 17:22:14 +0900

    fix: findAllUserComments

--- src/main/java/com/mountain/place/domain/comment/service/CommentService.java
+++ src/main/java/com/mountain/place/domain/comment/service/CommentService.java
@@ -2,9 +2,7 @@ package com.mountain.place.domain.comment.service;
 
 import com.mountain.place.controller.communitycomment.dto.RegisterCommuCommentDTO;
 import com.mountain.place.controller.mountainComment.dto.RegisterMTCommentDTO;
-import com.mountain.place.controller.mountainComment.dto.ResponseMTCommentDTO;
 import com.mountain.place.domain.category.dao.CategoryRepository;
-import com.mountain.place.domain.category.model.Category;
 import com.mountain.place.domain.comment.dao.CommentRespository;
 import com.mountain.place.domain.comment.model.Comment;
 import com.mountain.place.domain.community.dao.CommunityRepository;
@@ -12,19 +10,14 @@ import com.mountain.place.domain.community.model.Community;
 import com.mountain.place.domain.mountain.dao.MountainRepository;
 import com.mountain.place.domain.mountain.model.Mountain;
 import com.mountain.place.domain.user.model.User;
-import com.mountain.place.domain.user.dao.UserRepository;
 import com.mountain.place.exception.CustomException;
 import com.mountain.place.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Error;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.LongSummaryStatistics;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -42,8 +35,6 @@ public class CommentService {
     @Autowired
     MountainRepository mountainRepository;
 
-    @Autowired
-    UserRepository userRepository;
 
     @Transactional
     public Comment createComment(User user, Community community, RegisterCommuCommentDTO registerCommentDTO) {
@@ -86,26 +77,26 @@ public class CommentService {
             commentRespository.delete(comment);
         }
     }
-
+  
     public void updateCommunityComment(User user, Long commuPostNum, Long commentNo, RegisterCommuCommentDTO registerCommentDTO) {
 
-        Community community = communityRepository.findById(commuPostNum)
-                .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_COMMUNITY));
+      Community community = communityRepository.findById(commuPostNum)
+              .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_COMMUNITY));
 
-        Comment comment =
-                commentRespository.findById(commentNo)
-                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_REPLY));
+      Comment comment =
+              commentRespository.findById(commentNo)
+                      .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_REPLY));
 
-        if(!comment.getUser().getId().equals(user.getId())) {
-            throw new CustomException(ErrorCode.FORBIDDEN_USER);
-        } else if(!comment.getCommuNo().equals(community)){
-            throw new CustomException(ErrorCode.BAD_REQUEST_PARAM);
-        } else {
-            comment.setCommentContent(registerCommentDTO.getContent());
-            commentRespository.save(comment);
-        }
+      if(!comment.getUser().getId().equals(user.getId())) {
+          throw new CustomException(ErrorCode.FORBIDDEN_USER);
+      } else if(!comment.getCommuNo().equals(community)){
+          throw new CustomException(ErrorCode.BAD_REQUEST_PARAM);
+      } else {
+          comment.setCommentContent(registerCommentDTO.getContent());
+          commentRespository.save(comment);
+      }
 
-    }
+  }
 
     @Transactional
     public Comment createMTComment(User user, Long mountainNo, RegisterMTCommentDTO registerMTCommentDTO) {
@@ -157,14 +148,7 @@ public class CommentService {
             commentRespository.save(comment);
         }
     }
-    /**/
-    @Transactional
-    public Page<Comment> findAllUserComments(String userId, Pageable pageable) {
-        User user = userRepository.findById(userId)
-                .orElseThrow();// TODO: UserNotFoundException::new 추가하기
 
-        return commentRespository.findByUser(user, pageable);
-    }
 
 
     @Transactional
