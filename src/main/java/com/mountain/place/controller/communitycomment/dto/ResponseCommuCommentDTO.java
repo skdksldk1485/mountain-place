96ef94705c28e067c8845db087bf8c470aa2bb1e
skdksldk1485 <skdksldk1485@naver.com>
2022-03-12 17:19:56 +0900

    fix: ResponseCommuCommentDTO, ResponseMTCommentDTO

--- src/main/java/com/mountain/place/controller/communitycomment/dto/ResponseCommuCommentDTO.java
+++ src/main/java/com/mountain/place/controller/communitycomment/dto/ResponseCommuCommentDTO.java
@@ -1,8 +1,6 @@
 package com.mountain.place.controller.communitycomment.dto;
 
-import com.mountain.place.controller.category.dto.ResponseCategoryDTO;
 import com.mountain.place.controller.community.dto.CommunityDTO;
-import com.mountain.place.controller.community.dto.ResponseCommuDTO;
 import com.mountain.place.controller.user.dto.UserDTO;
 import com.mountain.place.domain.comment.model.Comment;
 import com.mountain.place.domain.community.model.Community;
@@ -27,10 +25,15 @@ public class ResponseCommuCommentDTO {
 
     public ResponseCommuCommentDTO(Comment comment, Community community) {
         this.commentNo = comment.getCommentNo();
-        this.user = new UserDTO(comment.getUid());
+        this.user = new UserDTO(comment.getUser());
         this.commentContent = comment.getCommentContent();
         this.createdAt = comment.getFstRegDtm();
         this.updateAt = comment.getLstUpdDtm();
         this.community = new CommunityDTO(community);
     }
 }
+
+
+
+
+
--- src/main/java/com/mountain/place/controller/mountainComment/dto/ResponseMTCommentDTO.java
+++ src/main/java/com/mountain/place/controller/mountainComment/dto/ResponseMTCommentDTO.java
@@ -1,10 +1,8 @@
 package com.mountain.place.controller.mountainComment.dto;
 
 
-import com.mountain.place.controller.community.dto.CommunityDTO;
 import com.mountain.place.controller.user.dto.UserDTO;
 import com.mountain.place.domain.comment.model.Comment;
-import com.mountain.place.domain.community.model.Community;
 import com.mountain.place.domain.mountain.model.Mountain;
 import lombok.Data;
 
@@ -15,23 +13,24 @@ public class ResponseMTCommentDTO {
 
     private Long commentNo;
 
+    private UserDTO user;
+
+    private Mountain mountainNo;
+
     private String commentContent;
 
     private LocalDateTime createdAt;
 
     private LocalDateTime updateAt;
 
-    private UserDTO user;
-
-    //private Mountain mountainNo;
 
     public ResponseMTCommentDTO(Comment mtComment) {
         this.commentNo = mtComment.getCommentNo();
-        this.user = new UserDTO(mtComment.getUid());
+        this.user = new UserDTO(mtComment.getUser());
         this.commentContent = mtComment.getCommentContent();
         this.createdAt = mtComment.getFstRegDtm();
         this.updateAt = mtComment.getLstUpdDtm();
-        //this.mountainNo = mtComment.getMountainNo();
+        this.mountainNo = mtComment.getMountainNo();
     }
 
 }
