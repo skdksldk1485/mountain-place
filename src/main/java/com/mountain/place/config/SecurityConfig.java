e832084eb75cf5ce0fcb65235889e2a95b513923
skdksldk1485 <skdksldk1485@naver.com>
2022-03-13 16:55:58 +0900

    fix: security ignore

--- src/main/java/com/mountain/place/config/SecurityConfig.java
+++ src/main/java/com/mountain/place/config/SecurityConfig.java
@@ -45,7 +45,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/categories/**")
                 .antMatchers(HttpMethod.GET, "/communities")
                 .antMatchers(HttpMethod.GET, "/communities/**")
-                .antMatchers("/**")
+                .antMatchers("/css/**")
+                .antMatchers("/static/**")
+                .antMatchers("/js/**")
+                .antMatchers("/img/**")
+                .antMatchers("/fonts/**")
+                .antMatchers("/vendor/**")
+                .antMatchers("/favicon.ico")
+                .antMatchers("/pages/**")
             ;
     }
 
